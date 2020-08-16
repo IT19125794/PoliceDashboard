deaa3527236516ddc9e0987ab153c7342b981581
IT19125794 <it19125794@my.sliit.lk>
2020-08-16 19:12:25 +0530

    Emergency Contact Edit

---
+++ app/src/main/java/com/example/policedashboard/ui/EmergencyContactsEdit/EmergencyEditFragment.java
@@ -0,0 +1,49 @@
+package com.example.policedashboard.ui.EmergencyContactsEdit;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.policedashboard.R;
+
+public class EmergencyEditFragment extends Fragment {
+
+    private EmergencyEditViewModel EmergencyEditViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        EmergencyEditViewModel =
+                ViewModelProviders.of(this).get(EmergencyEditViewModel.class);
+        View root = inflater.inflate(R.layout.emergency_contact_edit, container, false);
+        final TextView textView = root.findViewById(R.id.textView26);
+        final TextView textView1 = root.findViewById(R.id.textView27);
+        final TextView textView2 = root.findViewById(R.id.textView28);
+        final TextView textView3 = root.findViewById(R.id.textView29);
+        final TextView textView4 = root.findViewById(R.id.textView30);
+        final TextView textView5 = root.findViewById(R.id.textView31);
+        final TextView textView6 = root.findViewById(R.id.textView32);
+        final TextView textView7 = root.findViewById(R.id.textView33);
+        EmergencyEditViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+                textView1.setText(s);
+                textView2.setText(s);
+                textView3.setText(s);
+                textView4.setText(s);
+                textView5.setText(s);
+                textView6.setText(s);
+                textView7.setText(s);
+            }
+        });
+        return root;
+    }
+}
---
+++ app/src/main/java/com/example/policedashboard/ui/EmergencyContactsEdit/EmergencyEditViewModel.java
@@ -0,0 +1,19 @@
+package com.example.policedashboard.ui.EmergencyContactsEdit;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class EmergencyEditViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public EmergencyEditViewModel() {
+        mText = new MutableLiveData<>();
+    //    mText.setValue("This is gallery fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
